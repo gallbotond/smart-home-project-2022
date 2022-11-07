e9e3ea3d736fa0f47b404b2c573cb07f3e49a047
Gáll Botond <gallbotondofficial@gmail.com>
2022-11-07 14:48:52 +0200

    layout practice

--- src/app-frontend-expo/App.js
+++ src/app-frontend-expo/App.js
@@ -1,10 +1,16 @@
-import { View, Text } from "react-native";
+import { NavigationContainer, StackActions } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
+import HomeScreen from "./screens/HomeScreen";
 
-import TodoList from "./apps/TodoList";
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-      <TodoList />
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
