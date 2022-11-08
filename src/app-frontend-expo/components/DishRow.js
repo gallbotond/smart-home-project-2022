2bb485b6d5acfae2fcd3620d4e4160b69d7c6895
Gáll Botond <gallbotondofficial@gmail.com>
2022-11-08 12:08:20 +0200

    implement redux state management

--- src/app-frontend-expo/components/DishRow.js
+++ src/app-frontend-expo/components/DishRow.js
@@ -1,20 +1,47 @@
 import { View, Text, Image, TouchableOpacity } from "react-native";
 import React, { useState } from "react";
 import { urlFor } from "../sanity";
-// import Currency from 'react-currency-formatter'
+import Currency from "react-currency-formatter";
+import {
+  MinusCircleIcon,
+  PlusCircleIcon,
+} from "react-native-heroicons/outline";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  addToBasket,
+  removeFromBasket,
+  selectBasketItemsWithId,
+} from "../features/basketSlice";
 
 export default function DishRow({ id, name, description, price, image }) {
   const [isPressed, setIsPressed] = useState(false);
+  const items = useSelector((state) => selectBasketItemsWithId(state, id));
+  const dispatch = useDispatch();
+
+  const addItemToBasket = () => {
+    dispatch(addToBasket({ id, name, description, price, image }));
+  };
+
+  const removeItemFromBasket = () => {
+    if (!items.length > 0) return;
+
+    dispatch(removeFromBasket({ id }));
+  };
 
   return (
     <>
-      <TouchableOpacity className="bg-white border p-4 border-gray-200">
+      <TouchableOpacity
+        onPress={() => setIsPressed(!isPressed)}
+        className={`bg-white border p-4 border-gray-200 ${
+          isPressed && "border-b-0"
+        }`}
+      >
         <View className="flex-row">
           <View className="flex-1 pr-2">
             <Text className="text-lg mb-1">{name}</Text>
             <Text className="text-gray-400">{description}</Text>
             <Text className="text-gray-400 mt-2">
-              {/* <Currency quantity={price} currency="GBP" /> */}
+              <Currency quantity={price} currency="GBP" />
             </Text>
           </View>
 
@@ -27,6 +54,22 @@ export default function DishRow({ id, name, description, price, image }) {
           </View>
         </View>
       </TouchableOpacity>
+
+      {isPressed && (
+        <View className="bg-white px-4">
+          <View className="flex-row items-center space-x-2 pb-3">
+            <TouchableOpacity onPress={removeItemFromBasket}>
+              <MinusCircleIcon color="#00bbcc" size={40} />
+            </TouchableOpacity>
+
+            <Text>{items.length}</Text>
+
+            <TouchableOpacity onPress={addItemToBasket}>
+              <PlusCircleIcon color="#00bbcc" size={40} />
+            </TouchableOpacity>
+          </View>
+        </View>
+      )}
     </>
   );
 }
