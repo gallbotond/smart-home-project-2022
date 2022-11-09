1f7d8ba1a157cc512e542490c57abd3263b74aae
Gáll Botond <gallbotondofficial@gmail.com>
2022-11-09 16:42:11 +0200

    develop synch

---
+++ src/app-frontend-expo/slices/navSlice.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { createSlice } from '@reduxjs/toolkit'
+
+export default function navSlice() {
+  return (
+    <View>
+      <Text>navSlice</Text>
+    </View>
+  )
+}
